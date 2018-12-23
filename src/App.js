b3dc985b88d2f0ea2cb854aaaf74d8cc96cad901
CryptoLain <klabweb@tuta.io>
2018-12-23 15:41:42 -0600

    first push

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {urlList: ['/images/stocks/1.jpg',
+                        '/images/stocks/2.png',
+                        '/images/stocks/3.jpg',
+                        '/images/stocks/4.jpg',
+                        '/images/stocks/5.png',
+                        '/images/stocks/6.jpg',
+                        '/images/stocks/7.jpg',
+                        '/images/stocks/8.png',
+                        '/images/stocks/9.jpg',
+                        '/images/stocks/10.png'
+                        ],
+                   currentIndex: this.props.index,
+                   alt: "wojack"
+                  };
+    this.switchImage();   //starts time when class created
+  }
+
+  //incriments this.CurrentIndex by +1 until going to breach array range, then resets to 0
+  nextImage(){
+    if(this.state.currentIndex >= this.state.urlList.length-1)
+      this.setState({currentIndex: 0});
+    else
+      this.setState({currentIndex: this.state.currentIndex+1});
+  }
+
+  //updates image every second
+  switchImage(){
+        setInterval(()=> this.nextImage(), 3000);
+  }
+
+  //create image and set default photo to play
+  getImage(index){
+      let pic = <img src={this.state.urlList[this.state.currentIndex]} alt="wojack" style={{width:"100%", height: "100%", margin: "30px"}}></img>;
+
+      //different image sizes for larger and smaller viewports
+      if(document.documentElement.clientWidth > 900){
+        pic.props.style.width="20vw";
+        pic.props.style.height="20vw";
+      }
+      else{
+        pic.props.style.width="90vw";
+        pic.props.style.height="90vw";
+      }
+
+      return pic;
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return this.getImage();
   }
 }
+
 
 export default App;
---
+++ src/BoxGroup.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import App from './App';
+
+class AppGroup extends Component {
+
+  getImageGroup(){
+    let images = [];
+     //note using 10 is bad form and too static, but only way without changing array in App
+     //to be a JSON file both Components can access, etc
+    for(let i = 0; i < 10; i++){
+      images[i] = <App index={i}/>
+    }
+    return images;
+  }
+
+  render() {
+    return (
+      <div id="container" style={{display: "flex", flexWrap: "wrap"}}>
+         {this.getImageGroup().map(oneImage => <div style={{margin: "auto"}}>{oneImage}</div>)}
+      </div>
+    );
+  }
+}
+
+
+
+export default AppGroup;
