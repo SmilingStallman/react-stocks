ef9a065d2e85876e82da6ad267964a5ae7653634
miskellk <kyle.miskell@cdk.com>
2019-11-12 15:37:02 -0600

    build fix II

---
+++ build/precache-manifest.969db5f58b158edca7a4de7dff4f691d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388467c3a3fd9b62bc6646aee8c51f5e",
+    "url": "/react-stocks/index.html"
+  },
+  {
+    "revision": "39dac6741a21902ee2d8",
+    "url": "/react-stocks/static/css/main.9900add9.chunk.css"
+  },
+  {
+    "revision": "871498c6320a858295e8",
+    "url": "/react-stocks/static/js/2.36714e1a.chunk.js"
+  },
+  {
+    "revision": "39dac6741a21902ee2d8",
+    "url": "/react-stocks/static/js/main.8cc9097b.chunk.js"
+  },
+  {
+    "revision": "d7dd8ad0839d7dad26f2",
+    "url": "/react-stocks/static/js/runtime-main.f0728881.js"
+  }
+]);
