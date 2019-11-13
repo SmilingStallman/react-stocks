e3014130b6c7838f52acb5bfad546933476a40eb
miskellk <kyle.miskell@cdk.com>
2019-11-13 10:40:36 -0600

    production build creation

---
+++ build/precache-manifest.4e25b69bf0dd0d1fd48a2f5035a9ae36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a3adb5657765d95f86c4c61a7d4fbe8",
+    "url": "/react-stocks/index.html"
+  },
+  {
+    "revision": "101fa96aef6fcae08508",
+    "url": "/react-stocks/static/css/main.9900add9.chunk.css"
+  },
+  {
+    "revision": "871498c6320a858295e8",
+    "url": "/react-stocks/static/js/2.36714e1a.chunk.js"
+  },
+  {
+    "revision": "101fa96aef6fcae08508",
+    "url": "/react-stocks/static/js/main.30c90542.chunk.js"
+  },
+  {
+    "revision": "d7dd8ad0839d7dad26f2",
+    "url": "/react-stocks/static/js/runtime-main.f0728881.js"
+  }
+]);
--- src/Box.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Box = (url) => {
-  return  <img src={url} alt="wojack" style={{margin: "3vh", width:"41vh", height: "41vh"}}></img>;
-}
-
-export default Box;
